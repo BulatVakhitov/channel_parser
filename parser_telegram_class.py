134e92938489cfb2d5f0361bbaadf4db267a0fc4
0encodedguy0 <alexander.skachihin@gmail.com>
2024-04-13 21:31:32 +0300

    Changed Parser Class functions & added tests

--- parser_telegram_class.py
+++ parser_telegram_class.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from datetime import datetime
 
@@ -20,61 +21,62 @@ class DateTimeEncoder(json.JSONEncoder):
 
 class Telegram_Parser:
 
-    @classmethod
-    def client_initialize(cls):
-        api_id = int(input('Enter app_id: '))  # 11111111
-        api_hash = str(input('Enter app_hash: '))  # '11111111111111111111111111111111'
-        phone = str(input('Enter phone number: '))  # '11111111111'
-        return api_id, api_hash, phone
+    def __init__(self, api_id, api_hash, phone):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.phone = phone
+        self.client = self.client_start(phone=phone, api_id=api_id, api_hash=api_hash)
 
     @classmethod
-    def client_start(cls, api_id, api_hash, phone):
+    def client_start(cls, phone, api_id, api_hash):
         try:
-            client = TelegramClient(phone, api_id, api_hash, system_version="4.16.30-vxCUSTOM")
-            client.start()
+            client = TelegramClient(session=phone, api_id=api_id, api_hash=api_hash, system_version="4.16.30-vxCUSTOM")
+            client.start(phone=phone)
 
             return client
         except Exception:
-            raise Exception("Указан неправильный токен")
+            raise Exception("Неправильно введен токен")
 
-    def __init__(self):
-        self.api_id, self.api_hash, self.phone = self.client_initialize()
-        self.client = self.client_start(self.api_id, self.api_hash, self.phone)
-        pass
+    def parse_channel(self, url, path_to_json="channel_messages.json", offset_id=0, limit=100,
+                            total_count_limit=50):
+        try:
+            channel = self.client.get_entity(url)
 
-    def client_is_connected(self):
-        return self.client.is_connected()
+            all_messages = []
+            total_messages = 0
 
-#    async def parse_channel(self, url, path_to_json="channel_messages.json", offset_id=0, limit=100,
-#                            total_count_limit=50):
-#        channel = await client.get_entity(url)
-#
-#        all_messages = []
-#        total_messages = 0
-#
-#        while True:
-#            history = await client(GetHistoryRequest(
-#                peer=channel,
-#                offset_id=offset_id,
-#                offset_date=None,
-#                add_offset=0,
-#                limit=limit,
-#                max_id=0,
-#                min_id=0,
-#                hash=0
-#            ))
-#            if not history.messages:
-#                break
-#            messages = history.messages
-#            for message in messages:
-#                all_messages.append(message.to_dict())
-#            offset_id = messages[len(messages) - 1].id
-#            total_messages = len(all_messages)
-#            if total_count_limit != 0 and total_messages >= total_count_limit:
-#                break
+            while True:
+                history = self.client(GetHistoryRequest(
+                    peer=channel,
+                    offset_id=offset_id,
+                    offset_date=None,
+                    add_offset=0,
+                    limit=limit,
+                    max_id=0,
+                    min_id=0,
+                    hash=0
+                ))
+                if not history.messages:
+                    break
+                messages = history.messages
+                for message in messages:
+                    all_messages.append(message.to_dict())
+                offset_id = messages[len(messages) - 1].id
+                total_messages = len(all_messages)
+                if total_count_limit != 0 and total_messages >= total_count_limit:
+                    break
 
-#        with open("channel_messages.json", "w", encoding="UTF-8") as outfile:
-#            json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
+            with open(path_to_json, "w", encoding="UTF-8") as outfile:
+                json.dump(all_messages, outfile, ensure_ascii=False, cls=DateTimeEncoder)
+        except Exception:
+            raise Exception("Указан неверный url")
 
-#    async def client_disconnect(self):
-#        await client.disconnect()
+    def client_disconnect(self):
+        self.client.disconnect()
+
+
+#First time auth
+#parser = Telegram_Parser(11111111, '11111111111111111111111111111111', '11111111111')
+#print(parser.client.is_connected())
+#parser.client_disconnect()
+#parser.parse_channel(url="https://t.me/rian_ru")
